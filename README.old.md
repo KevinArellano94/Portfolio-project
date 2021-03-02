450d75531d600d246c498da7ff4554a8718b47ee
Kevin Arellano <Kevin.Arellano94@gmail.com>
2021-03-02 02:59:44 -0500

    .
    .

--- README.old.md
+++ README.old.md
@@ -1,3 +1,6 @@
 # npm cache clean --force
 # npm install -g create-react-app
 # create-react-app .
+
+# npm install react-router-dom
+# npm audit fix --force
--- src/App.js
+++ src/App.js
@@ -1,6 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+/*
 function App() {
   return (
     <div className="App">
@@ -15,7 +69,15 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Kevin Arellano
+          <b>KEVIN ARELLANO</b>
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          react.js
         </a>
       </header>
     </div>
@@ -23,3 +85,4 @@ function App() {
 }
 
 export default App;
+*/
